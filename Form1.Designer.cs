d087fba844d40717927462d2322dfab6650a47cc
Zwelious <112923618+Zwelious@users.noreply.github.com>
2023-03-12 21:24:53 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace THA_W3_Alfred_W
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.age = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.phone = new System.Windows.Forms.TextBox();
+            this.submit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(55, 42);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(44, 16);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Name";
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Location = new System.Drawing.Point(55, 88);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(32, 16);
+            this.ageLabel.TabIndex = 1;
+            this.ageLabel.Text = "Age";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(55, 133);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(41, 16);
+            this.emailLabel.TabIndex = 2;
+            this.emailLabel.Text = "Email";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Location = new System.Drawing.Point(55, 175);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(97, 16);
+            this.phoneLabel.TabIndex = 3;
+            this.phoneLabel.Text = "Phone Number";
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(184, 42);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(100, 22);
+            this.name.TabIndex = 4;
+            // 
+            // age
+            // 
+            this.age.Location = new System.Drawing.Point(184, 85);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(100, 22);
+            this.age.TabIndex = 5;
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(184, 130);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(100, 22);
+            this.email.TabIndex = 6;
+            // 
+            // phone
+            // 
+            this.phone.Location = new System.Drawing.Point(184, 172);
+            this.phone.Name = "phone";
+            this.phone.Size = new System.Drawing.Size(100, 22);
+            this.phone.TabIndex = 7;
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(58, 221);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 23);
+            this.submit.TabIndex = 8;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.phone);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.age);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox age;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.TextBox phone;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace THA_W3_Alfred_W
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            char inputAge = Convert.ToChar(age.Text);
+            int umur = -1;
+            bool invalid = false;
+            if (Char.IsNumber(inputAge))
+            {
+                umur = Convert.ToInt32(inputAge);
+            }
+            else
+            {
+                invalid = true;
+                MessageBox.Show("Age must be a number.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            string ageMessage = "";
+            if(umur > 18){
+                ageMessage = "You are an adult";
+            }
+            else if(umur < 18 && umur >= 0)
+            {
+                ageMessage = "You are a minor";
+            }
+            
+            if(invalid == false){
+                string message = "Name: " + name.Text + " Email: " + email.Text + " Phone Number: " + phone.Text + ". " + ageMessage;
+                MessageBox.Show(message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            name.Text = "";
+            age.Text = "";
+            email.Text = "";
+            phone.Text = "";
+        }
+    }
+}
